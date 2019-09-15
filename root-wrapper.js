5f4298482227ea2ba69b038bfde08d0d77debb54
spences10 <spences10apps@gmail.com>
2019-09-15 10:56:37 +0100

    use highlight component from prism react renderer

--- root-wrapper.js
+++ root-wrapper.js
@@ -1,4 +1,6 @@
 import { MDXProvider } from '@mdx-js/react'
+import Highlight, { defaultProps } from 'prism-react-renderer'
+import theme from 'prism-react-renderer/themes/nightOwl'
 import React from 'react'
 
 const components = {
@@ -8,6 +10,41 @@ const components = {
   'p.inlineCode': props => (
     <code {...props} style={{ backgroundColor: 'lightgrey' }}></code>
   ),
+  pre: props => {
+    const className = props.children.props.className || ''
+    const matches = className.match(/language-(?<lang>.*)/)
+
+    return (
+      <Highlight
+        {...defaultProps}
+        code={props.children.props.children.trim()}
+        language={
+          matches && matches.groups && matches.groups.lang
+            ? matches.groups.lang
+            : ''
+        }
+        theme={theme}
+      >
+        {({
+          className,
+          style,
+          tokens,
+          getLineProps,
+          getTokenProps,
+        }) => (
+          <pre className={className} style={style}>
+            {tokens.map((line, i) => (
+              <div {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => (
+                  <span {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ))}
+          </pre>
+        )}
+      </Highlight>
+    )
+  },
 }
 
 export const wrapRootElement = ({ element }) => (
