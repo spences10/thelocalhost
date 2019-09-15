0eb8661a573bb294e139f24e08833a6eb10109e7
spences10 <spences10apps@gmail.com>
2019-09-15 08:21:09 +0100

    add previous/next navigation

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,55 @@
 const { createFilePath } = require(`gatsby-source-filesystem`)
+const path = require(`path`)
+
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions
+  const blogPostTemplate = path.resolve(
+    'src/templates/blogPostTemplate.js'
+  )
+
+  return graphql(`
+    {
+      allMdx(
+        sort: { fields: [frontmatter___date], order: DESC }
+        filter: { frontmatter: { published: { eq: true } } }
+      ) {
+        nodes {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            title
+            date
+          }
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      throw result.errors
+    }
+
+    const posts = result.data.allMdx.nodes
+
+    posts.forEach((post, index) => {
+      const previous =
+        index === post.length - 1 ? null : posts[index + 1]
+      const next = index === 0 ? null : posts[index - 1]
+
+      createPage({
+        path: post.fields.slug,
+        component: blogPostTemplate,
+        context: {
+          slug: post.fields.slug,
+          previous,
+          next,
+        },
+      })
+    })
+  })
+}
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
---
+++ src/templates/blogPostTemplate.js
@@ -0,0 +1,48 @@
+import { graphql, Link } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
+import React from 'react'
+import Dump from '../components/Dump'
+import { Layout } from '../components/Layout'
+
+export default ({ data, pageContext }) => {
+  const { frontmatter, body } = data.mdx
+  const { previous, next } = pageContext
+  return (
+    <Layout>
+      <Dump previous={previous}></Dump>
+      <h1>{frontmatter.title}</h1>
+      <p>{frontmatter.date}</p>
+      <MDXRenderer>{body}</MDXRenderer>
+      {previous === false ? null : (
+        <>
+          {previous && (
+            <Link to={previous.fields.slug}>
+              <p>{previous.frontmatter.title}</p>
+            </Link>
+          )}
+        </>
+      )}
+      {next === false ? null : (
+        <>
+          {next && (
+            <Link to={next.fields.slug}>
+              <p>{next.frontmatter.title}</p>
+            </Link>
+          )}
+        </>
+      )}
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query PostBySlug($slug: String!) {
+    mdx(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        date
+      }
+      body
+    }
+  }
+`
