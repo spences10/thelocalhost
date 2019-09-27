812d4fda30d9b6c310d33122815ec79354fda094
spences10 <spences10apps@gmail.com>
2019-09-27 06:19:16 +0100

    use copy button in code component

--- src/components/pageElements/Code.js
+++ src/components/pageElements/Code.js
@@ -2,8 +2,10 @@ import Highlight, { defaultProps } from 'prism-react-renderer'
 import theme from 'prism-react-renderer/themes/nightOwl'
 import React from 'react'
 import styled from 'styled-components'
+import { copyToClipboard } from '../../utils/copy-to-clipboard'
 
 const Pre = styled.pre`
+  position: relative;
   text-align: left;
   margin: 1em 0;
   padding: 0.5em;
@@ -24,7 +26,22 @@ const LineNo = styled.span`
   opacity: 0.3;
 `
 
+const CopyCode = styled.button`
+  position: absolute;
+  right: 0.25rem;
+  border: 0;
+  border-radius: 3px;
+  margin: 0.25em;
+  opacity: 0.3;
+  &:hover {
+    opacity: 1;
+  }
+`
+
 export const Code = ({ codeString, language, ...props }) => {
+  const handleClick = () => {
+    copyToClipboard(codeString)
+  }
   return (
     <Highlight
       {...defaultProps}
@@ -40,6 +57,7 @@ export const Code = ({ codeString, language, ...props }) => {
         getTokenProps,
       }) => (
         <Pre className={className} style={style}>
+          <CopyCode onClick={handleClick}>Copy</CopyCode>
           {tokens.map((line, i) => (
             <div {...getLineProps({ line, key: i })}>
               <LineNo>{i + 1}</LineNo>
