f366e02a0c0106f048be15899f88dd246ee92552
spences10 <spences10apps@gmail.com>
2019-09-27 06:01:08 +0100

    style code component

--- src/components/pageElements/Code.js
+++ src/components/pageElements/Code.js
@@ -1,6 +1,28 @@
 import Highlight, { defaultProps } from 'prism-react-renderer'
 import theme from 'prism-react-renderer/themes/nightOwl'
 import React from 'react'
+import styled from 'styled-components'
+
+const Pre = styled.pre`
+  text-align: left;
+  margin: 1em 0;
+  padding: 0.5em;
+  overflow-x: auto;
+  border-radius: 3px;
+
+  & .token-lline {
+    line-height: 1.3em;
+    height: 1.3em;
+  }
+  font-family: 'Courier New', Courier, monospace;
+`
+
+const LineNo = styled.span`
+  display: inline-block;
+  width: 2em;
+  user-select: none;
+  opacity: 0.3;
+`
 
 export const Code = ({ codeString, language, ...props }) => {
   return (
@@ -17,15 +39,16 @@ export const Code = ({ codeString, language, ...props }) => {
         getLineProps,
         getTokenProps,
       }) => (
-        <pre className={className} style={style}>
+        <Pre className={className} style={style}>
           {tokens.map((line, i) => (
             <div {...getLineProps({ line, key: i })}>
+              <LineNo>{i + 1}</LineNo>
               {line.map((token, key) => (
                 <span {...getTokenProps({ token, key })} />
               ))}
             </div>
           ))}
-        </pre>
+        </Pre>
       )}
     </Highlight>
   )
