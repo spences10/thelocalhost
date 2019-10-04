1a65ed6d9befaa9a6f1ed953cfa6e3b69f506014
spences10 <spences10apps@gmail.com>
2019-10-04 06:36:47 +0100

    add index images query

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import { Link } from 'gatsby'
+import Img from 'gatsby-image'
 import React from 'react'
 import styled from 'styled-components'
 import { Layout } from '../components/Layout'
@@ -6,6 +7,10 @@ import { Layout } from '../components/Layout'
 const IndexWrapper = styled.main``
 
 const PostWrapper = styled.div``
+
+const Image = styled(Img)`
+  border-radius: 5px;
+`
 
 export default ({ data }) => {
   return (
@@ -16,6 +21,11 @@ export default ({ data }) => {
           ({ id, excerpt, frontmatter, fields }) => (
             <PostWrapper key={id}>
               <Link to={fields.slug}>
+                {!!frontmatter.cover ? (
+                  <Image
+                    sizes={frontmatter.cover.childImageSharp.sizes}
+                  />
+                ) : null}
                 <h1>{frontmatter.title}</h1>
                 <p>{frontmatter.date}</p>
                 <p>{excerpt}</p>
@@ -39,7 +49,15 @@ export const query = graphql`
         excerpt(pruneLength: 250)
         frontmatter {
           title
-          date
+          date(formatString: "YYYY MMMM Do")
+          cover {
+            publicURL
+            childImageSharp {
+              sizes(maxWidth: 2000, traceSVG: { color: "#639" }) {
+                ...GatsbyImageSharpSizes_tracedSVG
+              }
+            }
+          }
         }
         fields {
           slug
