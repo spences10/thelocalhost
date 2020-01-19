afaab1dfbc71c5bcfaa4de8db05234bae1d32d94
spences10 <spences10apps@gmail.com>
2020-01-19 20:12:09 +0000

    add primary colours, font size and weight

--- src/theme/global-style.js
+++ src/theme/global-style.js
@@ -8,6 +8,18 @@ export const theme = {
     black: '#000',
     white: '#fff',
 
+    primary: {
+      100: '#aa7fd4',
+      200: '#9966cc',
+      300: '#884cc3',
+      400: '#773bb2',
+      500: '#663399',
+      600: '#552b80',
+      700: '#442266',
+      800: '#331a4d',
+      900: '#221133',
+    },
+
     grey: {
       100: '#f7fafc',
       200: '#edf2f7',
@@ -20,10 +32,47 @@ export const theme = {
       900: '#1a202c',
     },
   },
-  fonts: {
+  font: {
     sans: 'Poppins, sans-serif',
     serif: 'Pridi, sans',
     monospace: '"Space Mono", monospace',
+  },
+  fontSize: {
+    xs: '0.75rem',
+    sm: '0.875rem',
+    base: '1rem',
+    lg: '1.125rem',
+    xl: '1.25rem',
+    '2xl': '1.5rem',
+    '3xl': '1.875rem',
+    '4xl': '2.25rem',
+    '5xl': '3rem',
+    '6xl': '4rem',
+  },
+  fontWeight: {
+    hairline: '100',
+    thin: '200',
+    light: '300',
+    normal: '400',
+    medium: '500',
+    semibold: '600',
+    bold: '700',
+    extrabold: '800',
+    black: '900',
+  },
+  boxShadow: {
+    default:
+      '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
+    md:
+      '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+    lg:
+      '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+    xl:
+      '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
+    '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
+    inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
+    outline: '0 0 0 3px rgba(66, 153, 225, 0.5)',
+    none: 'none',
   },
 }
 
@@ -36,7 +85,7 @@ export const GlobalStyle = createGlobalStyle`
   html {
     box-sizing: border-box;
     scroll-behavior: smooth;
-    font-family: ${({ theme }) => theme.fonts.sans};
+    font-family: ${({ theme }) => theme.font.sans};
     color: ${({ theme }) => theme.colours.grey[900]};
   }
   body {
