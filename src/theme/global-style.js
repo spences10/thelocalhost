0fe1db4efeb260d4a618fcc2c58feefc7af28bea
spences10 <spences10apps@gmail.com>
2020-01-20 07:29:51 +0000

    add more theme options

--- src/theme/global-style.js
+++ src/theme/global-style.js
@@ -74,6 +74,48 @@ export const theme = {
     outline: '0 0 0 3px rgba(66, 153, 225, 0.5)',
     none: 'none',
   },
+  letterSpacing: {
+    tighter: '-0.05em',
+    tight: '-0.025em',
+    normal: '0',
+    wide: '0.025em',
+    wider: '0.05em',
+    widest: '0.1em',
+  },
+  lineHeight: {
+    none: '1',
+    tight: '1.25',
+    snug: '1.375',
+    normal: '1.5',
+    relaxed: '1.625',
+    loose: '2',
+  },
+  listStyleType: {
+    none: 'none',
+    disc: 'disc',
+    decimal: 'decimal',
+  },
+  spacing: {
+    px: '1px',
+    '0': '0',
+    '1': '0.25rem',
+    '2': '0.5rem',
+    '3': '0.75rem',
+    '4': '1rem',
+    '5': '1.25rem',
+    '6': '1.5rem',
+    '8': '2rem',
+    '10': '2.5rem',
+    '12': '3rem',
+    '16': '4rem',
+    '20': '5rem',
+    '24': '6rem',
+    '32': '8rem',
+    '40': '10rem',
+    '48': '12rem',
+    '56': '14rem',
+    '64': '16rem',
+  },
 }
 
 export const GlobalStyle = createGlobalStyle`
@@ -89,6 +131,7 @@ export const GlobalStyle = createGlobalStyle`
     color: ${({ theme }) => theme.colours.grey[900]};
   }
   body {
-    line-height:1.9;
+    line-height: ${({ theme }) => theme.lineHeight.normal};
+    letter-spacing: ${({ theme }) => theme.normal};
   }
 `
