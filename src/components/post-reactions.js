1fb654d1a2bd13d43f6fd98a602e3cc240683183
spences10 <spences10apps@gmail.com>
2020-01-26 17:16:53 +0000

    add assets and empty file for post reactions

---
+++ src/components/post-reactions.js
@@ -0,0 +1,9 @@
+// What do you think?
+/**
+ * Nice! thumbs-up.png
+ * Love it! love.png
+ * No good yo! angry.png
+ * Can't even! surprised.png
+ * LOL! funny.png
+ */
+// animation: anim-hop 1s ease-in-out infinite alternate;
