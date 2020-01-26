f4380622a7a89234eaa9884a68f9573aad8ad942
spences10 <spences10apps@gmail.com>
2020-01-26 15:47:50 +0000

    remove margin on p in a blockquote

--- src/components/page-elements/blockquote.js
+++ src/components/page-elements/blockquote.js
@@ -2,13 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledText = styled.blockquote`
+  margin: 20px;
   p {
+    margin-top: 0;
     border-left: 5px solid ${({ theme }) => theme.colours.grey[700]};
     padding-left: 5px;
     font-style: italic;
     font-size: ${({ theme }) => theme.fontSize['2xl']};
     color: ${({ theme }) => theme.colours.grey[700]};
-    margin: 20px;
     word-break: break-word;
   }
 `;
