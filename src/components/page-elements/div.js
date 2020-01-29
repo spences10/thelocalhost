8b1f2e1c1e83f2c70bfe10603dd459773cea0ba6
spences10 <spences10apps@gmail.com>
2020-01-29 21:23:44 +0000

    remove videos for now

--- src/components/page-elements/div.js
+++ src/components/page-elements/div.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export const StyledDiv = styled.div``;
+export const StyledDiv = styled.div`
+  iframe {
+    display: none;
+  }
+`;
 
 export const Div = (props, { children }) => {
   return <StyledDiv {...props}>{children}</StyledDiv>;
