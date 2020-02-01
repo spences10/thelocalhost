5d67eb87887826b5dba28c1b79b8de115beec5ad
spences10 <spences10apps@gmail.com>
2020-02-01 05:55:39 +0000

    - add and config styled-breakpoints

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { down, up } from 'styled-breakpoints';
 import styled from 'styled-components';
 import { useSiteMetadata } from '../hooks/use-site-metadata';
 import { Header } from './header';
 
 const AppStyles = styled.main`
-  max-width: 680px;
+  max-width: 640px;
   margin: 0 auto;
   padding: 0 20px;
+  ${down('sm')} {
+    /* background-color: seagreen; */
+  }
+  ${up('md')} {
+    /* background-color: dodgerblue; */
+  }
+  ${up('lg')} {
+    /* background-color: rebeccapurple; */
+  }
+  ${up('xl')} {
+    /* background-color: hotpink; */
+  }
 `;
 
 export const Layout = ({ children }) => {
