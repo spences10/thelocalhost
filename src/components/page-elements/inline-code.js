be4683baee95437347969d1fd5240fbdcacbafa3
spences10 <spences10apps@gmail.com>
2020-02-07 14:15:24 +0000

    move inline code to css variable for headings

--- src/components/page-elements/inline-code.js
+++ src/components/page-elements/inline-code.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import 'victormono';
 
-const StyledText = styled.code`
+export const inlineCode = css`
   padding: 0 3px;
   background-color: ${({ theme }) => theme.colours.grey[400]};
   font-family: 'Victor Mono', monospace;
   font-size: ${({ theme }) => theme.fontSize.base};
   border-radius: ${({ theme }) => theme.borderRadius.sm};
+`;
+
+const StyledText = styled.code`
+  ${inlineCode}
 `;
 
 export const InlineCode = ({ children }) => {
