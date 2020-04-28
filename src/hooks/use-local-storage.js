a5a0e23b7b5081cbbcbe5612644c8450d80984eb
spences10 <spences10apps@gmail.com>
2020-04-28 13:22:22 +0100

    add use local storage hook

---
+++ src/hooks/use-local-storage.js
@@ -0,0 +1,37 @@
+// https://www.youtube.com/watch?v=BAf0uhKVHBk
+import { useEffect, useState } from 'react'
+
+export const useLocalState = (key, defaultValue) => {
+  const [value, setValue] = useState(() => {
+    const storedValue = localStorage.getItem(key)
+    return storedValue === null
+      ? defaultValue
+      : JSON.parse(storedValue)
+  })
+
+  useEffect(() => {
+    const listener = e => {
+      if (e.storedArea === localStorage && e.key === key) {
+        setValue(JSON.parse(e.newValue))
+      }
+    }
+    window.addEventListener('storage', listener)
+
+    return () => {
+      window.removeEventListener('storage', listener)
+    }
+  }, [key])
+
+  const setValueInLocalStorage = newValue => {
+    setValue(currentValue => {
+      const result =
+        typeof newValue === 'function'
+          ? newValue(currentValue)
+          : newValue
+      localStorage.setItem(key, JSON.stringify(result))
+      return result
+    })
+  }
+
+  return [value, setValueInLocalStorage]
+}
