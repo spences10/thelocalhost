dd5d738dfa2344acb4f6c7c74928afcd5180f6fd
spences10 <spences10apps@gmail.com>
2020-05-02 16:12:43 +0100

    add styled image

---
+++ src/components/page-elements/img.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledImg = styled.img`
+  &:hover {
+    opacity: 0.5;
+  }
+`
+
+export const Img = props => {
+  const { children, ...rest } = props
+
+  return (
+    <a href={props.src} target="_blank" rel="noopener noreferrer">
+      <StyledImg {...rest}>{children}</StyledImg>
+    </a>
+  )
+}
